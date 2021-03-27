b9a2011cff48770e014a105929aa43a7d5150638
coderaulia <auliasw89@gmail.com>
2021-03-28 01:47:49 +0700

    adding update and get models

--- dev/application/core/MY_Model.php
+++ dev/application/core/MY_Model.php
@@ -18,9 +18,34 @@ class MY_Model extends CI_Model
   // Batch untuk menginsert query lebih dari sekali
   public function insert($data, $batch = FALSE)
   {
-    if($batch == TRUE){
+    if ($batch == TRUE) {
       // Memanggil prefix dari database.php, dan memilih nama tabel dan menginsert data
-      $this->db->_insert_batch('{PRE}'.$this->_table_name, $data)
+      $this->db->_insert_batch('{PRE}' . $this->_table_name, $data);
+    } else {
+      // Insert tanpa batch atau berulang kali
+      $this->db->set($data);
+      $this->db->insert('{PRE}' . $this->_table_name, $data);
+      // Last inserted ID
+      $id = $this->db->insert_id();
+      return $id;
+    }
+  }
+
+  public function update($data, $where = array())
+  {
+    $this->db->set($data);
+    $this->db->where($where);
+    $this->db->update('{PRE}' . $this->_table_name, $data);
+  }
+
+  public function get($id = NULL, $single = FALSE)
+  {
+    if ($id != NULL) {
+      $filter = $this->_primary_filter;
+      $id = $filter($id);
+      $this->db->where($this->_primary_key, $id);
+      $method = 'row';
+    } elseif ($single == TRUE) {
     }
   }
 }
