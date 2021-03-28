87ed659637bd90ef92a3cd831f8556d1e6a3920a
coderaulia <auliasw89@gmail.com>
2021-03-28 14:40:55 +0700

    reconfig the configuration file

--- configuration.php
+++ configuration.php
@@ -5,9 +5,12 @@ class SConfig
   var $_site_url = "http://localhost/ci-cms/";
   var $_document_root = "/opt/lamp/htdocs/ci-cms";
   var $_host_name = "localhost";
+  var $_site_name = "auliasatriow.com";
   var $_database_name = "ci-cms";
   var $_database_user = "root";
   var $_database_password = "";
   var $_table_prefix = "tbl_";
-  var $_backend_template = "default";
+  var $_cms_name = "CI Basic CMS";
+  var $_backend_perpage = 10;
+  var $_frontend_perpage = 5;
 }
